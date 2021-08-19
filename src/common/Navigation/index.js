5638e9366f90740a8cc59031faf8d0a4447f03f6
Kinga Parasiewicz <kingaparasiewicz1997@gmail.com>
2021-08-19 10:11:46 +0200

    created navigation component with HTML only

---
+++ src/common/Navigation/index.js
@@ -0,0 +1,20 @@
+export const Navigation = () => {
+  return (
+    <>
+      <nav>
+        <header>
+          <h2>Ribollita</h2>
+          <p>Italian Restaurant</p>
+        </header>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Menu</li>
+          <li>Gallery</li>
+          <li>Contact</li>
+        </ul>
+        <button>Book now</button>
+      </nav>
+    </>
+  );
+};
