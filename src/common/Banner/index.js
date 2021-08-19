058d4a5e06e846ef01da160243494bd2812fa170
Kinga Parasiewicz <kingaparasiewicz1997@gmail.com>
2021-08-19 10:12:08 +0200

    created Banner component with HTML

---
+++ src/common/Banner/index.js
@@ -0,0 +1,17 @@
+
+
+export const Banner = () => {
+
+    return(
+        <>
+        <section>
+            <h1>Welcome</h1>
+            <p>Our restaurant offers full-service dining with breathtaking views 
+                seen from our indoor covered patio and our outdoor sundeck.
+            </p>
+            <button>Meet us</button>
+        </section>
+        </>
+
+    )
+}
