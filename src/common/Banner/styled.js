44fbb9e4c40404a988b1318b6181f9bcaf65eaf2
Kinga Parasiewicz <kingaparasiewicz1997@gmail.com>
2021-08-19 12:06:06 +0200

    created styled components

---
+++ src/common/Banner/styled.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import banner from "./../../assets/backg.jpg";
+
+export const Section = styled.section`
+  background: url(${banner});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  height: 80vh;
+  margin-top: 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+`;
+
+export const SectionName = styled.h1`
+  margin-top: 200px;
+  font-size: 8em;
+  color: black;
+  margin-bottom: 20px;
+  margin-left: -200px;
+  z-index: 2;
+`;
+
+export const SectionParagraph = styled.p`
+  color: black;
+  font-size: 20px;
+  max-width: 500px;
+  margin-left: -180px;
+  margin-bottom: 50px;
+  z-index: 2;
+`;
