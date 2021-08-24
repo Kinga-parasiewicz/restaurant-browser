97ef9ac33a3ac8d4f05c5587424c004f0e0a36ae
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-08-24 12:52:31 +0200

    create styles

--- src/features/About/styled.js
+++ src/features/About/styled.js
@@ -1,1 +1,23 @@
 import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  background: #dbdbdc;
+  margin-top: 20px;
+`;
+
+export const WrapperInner = styled.div`
+  padding-top: 150px;
+  padding-bottom: 50px;
+  display: flex;
+  justify-content: space-evenly;
+  margin-bottom: 50px;
+`;
+
+export const Name = styled.h2`
+  font-size: 34px;
+`;
+
+export const Description = styled.p`
+  max-width: 500px;
+  font-size: 18px;
+`;
