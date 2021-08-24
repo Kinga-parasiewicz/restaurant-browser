1d51ba4a09fdc5434ba433515ecc351bf0788cbd
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-08-24 12:52:21 +0200

    add image and styles and create structure component

--- src/features/About/index.js
+++ src/features/About/index.js
@@ -1,3 +1,22 @@
+import { Wrapper, WrapperInner, Name, Description } from "./styled";
+import restaurant from "./../../assets/restaurant.jpg";
+
 export const About = () => {
-  return <></>;
+  return (
+    <Wrapper>
+      <WrapperInner>
+        <img src={restaurant} alt="restaurant" />
+        <div>
+          <Name>About Us</Name>
+          <Description>
+            Anolini is a family owned and operated Italian Restaurant offering a
+            combination of fresh ingredients and authentic Italian cooking. We
+            will make sure you are served the most authentic and fresh Italian
+            dishes, while offering the best customer service. Our kitchen is
+            committed to providing our guests with the best Italian Cuisine.
+          </Description>
+        </div>
+      </WrapperInner>
+    </Wrapper>
+  );
 };
