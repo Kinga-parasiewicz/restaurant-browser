ae927e5b7616e7dc4a17b012c864b4bbcaacf090
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-08-24 15:53:16 +0200

    add footer to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Navigation } from "./common/Navigation/index";
 import { Banner } from "./common/Banner/index";
 import { Form } from "./features/Form";
 import { About } from "./features/About";
+import { Footer } from "./common/Footer";
 
 function App() {
   return (
@@ -9,9 +10,8 @@ function App() {
       <Navigation />
       <Banner />
       <Form/>
-      
       <About/>
-
+      <Footer/>
     </>
   );
 }
