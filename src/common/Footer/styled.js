9a183f6f17b1f5c5e6ae73f9981b2156329bbe61
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-08-24 15:53:04 +0200

    create styles

---
+++ src/common/Footer/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+  background: #323539;
+  padding: 70px;
+  text-align: center;
+`;
+
+export const Image = styled.img`
+  height: 47px;
+  margin-left: 20px;
+`;
