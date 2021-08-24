a3855e795aac1b3708960634e200a17f30872908
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-08-24 12:18:46 +0200

    add Form to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Navigation } from "./common/Navigation/index";
 import { Banner } from "./common/Banner/index";
+import { Form } from "./features/Form";
 
 function App() {
   return (
     <>
       <Navigation />
       <Banner />
+      <Form/>
+
     </>
   );
 }
