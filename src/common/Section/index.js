c35fd6c58e2d64424ae2a372e85fd37cb5183d50
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-08-25 14:09:55 +0200

    delete props

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -1,32 +1,36 @@
 import ravioli from "./../../assets/ravioli.jpg";
 import gelato from "./../../assets/gelato.jpg";
 import blackPasta from "./../../assets/black-pasta-scaled.jpg";
-import { Image , ImagesBox, Wrapper, LastImage, Description, NameImages} from "./styled"
-import { Name} from "./../../features/About/styled";
+import {
+  Image,
+  ImagesBox,
+  Wrapper,
+  LastImage,
+  Description,
+  NameImages,
+} from "./styled";
+import { Name } from "./../../features/About/styled";
 
 export const Section = () => (
   <Wrapper>
     <Name>Featured offers</Name>
-    <Description unlimited>
+    <Description>
       We offer a great variety of the best Italian dishes to our visitors and
       guests. Below are some of our most popular main dishes and desserts.
     </Description>
     <ImagesBox>
-    <div>
-        <Image src={ravioli} alt="ravioli"/>
-        <NameImages> Ravioli
-        </NameImages>
-    </div>
-    <div>
-        <Image src={blackPasta} alt="black pasta"/>
-        <NameImages>Black Pasta
-        </NameImages>
-    </div>
-    <div>
-        <LastImage src={gelato} alt="gelato"/>
-        <NameImages>Ice cream
-        </NameImages>
-    </div>
+      <div>
+        <Image src={ravioli} alt="ravioli" />
+        <NameImages> Ravioli</NameImages>
+      </div>
+      <div>
+        <Image src={blackPasta} alt="black pasta" />
+        <NameImages>Black Pasta</NameImages>
+      </div>
+      <div>
+        <LastImage src={gelato} alt="gelato" />
+        <NameImages>Ice cream</NameImages>
+      </div>
     </ImagesBox>
   </Wrapper>
 );
