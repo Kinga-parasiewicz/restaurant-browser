d1e6b6898e0ad00ea1ed83d08d9f53617e100e80
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-08-25 14:08:35 +0200

    add styles to Section

---
+++ src/common/Section/styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Image = styled.img`
+max-width:400px;
+`;
+export const LastImage= styled.img`
+width:355px;
+`;
+export const ImagesBox = styled.div`
+display:flex;
+flex-direction:row;
+justify-content:space-between;
+margin-bottom:50px;
+`;
+
+export const Wrapper = styled.section`
+margin:0 auto;
+max-width:1200px;
+text-align:center;
+`;
+
+export const Description = styled.p`
+  font-size: 18px;
+  line-height: 1.5;
+  text-align:center;
+  color:#71726E;
+  margin:30px;
+  `;
+export const NameImages = styled.p`
+color:#0914CE;
+font-size:22px;
+`;
+
+export const SingleImageBox = styled.div`
+`;
