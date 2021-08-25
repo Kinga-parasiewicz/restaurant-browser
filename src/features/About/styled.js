1eeac1678ae4141ff0dd803ad29d31b42d29315e
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-08-25 15:21:18 +0200

    add css and additional styles

--- src/features/About/styled.js
+++ src/features/About/styled.js
@@ -1,8 +1,12 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Wrapper = styled.section`
   background: #EFEEEB;
   margin-top: 20px;
+
+${({aboutWrapper}) => aboutWrapper && css `
+margin-top:0px;
+`}
 `;
 
 export const WrapperInner = styled.div`
@@ -14,15 +18,24 @@ export const WrapperInner = styled.div`
 `;
 
 export const Name = styled.h2`
-  font-size: 34px;
+  font-size: 40px;
+  text-align:center;
 `;
 
-export const Description = styled.p`
+export const DescriptionParagraph = styled.p`
   max-width: 500px;
-  font-size: 18px;
+  font-size: 19px;
   line-height: 1.5;
 `;
 export const DescriptionBox = styled.div`
   margin-left: 70px;
   text-align: center;
 `;
+
+export const ImagesSvg = styled.img`
+height:60px;`;
+
+export const GridBox = styled.section`
+display: grid;
+grid-template-columns:repeat(3, 1fr);`
+
