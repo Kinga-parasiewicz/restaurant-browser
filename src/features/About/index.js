7a90c7857ec6a98af9e8f9b882a21b3cb8a793d2
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-08-25 15:21:55 +0200

    rename paragraph

--- src/features/About/index.js
+++ src/features/About/index.js
@@ -1,4 +1,4 @@
-import { Wrapper, WrapperInner, Name, Description, DescriptionBox } from "./styled";
+import { Wrapper, WrapperInner, Name, DescriptionParagraph, DescriptionBox } from "./styled";
 import restaurant from "./../../assets/restaurant.jpg";
 import { StyledButton } from "./../../common/Button";
 
@@ -9,13 +9,13 @@ export const About = () => {
         <img src={restaurant} alt="restaurant" />
         <DescriptionBox>
           <Name>About Us</Name>
-          <Description>
+          <DescriptionParagraph>
             Anolini is a family owned and operated Italian Restaurant offering a
             combination of fresh ingredients and authentic Italian cooking. We
             will make sure you are served the most authentic and fresh Italian
             dishes, while offering the best customer service. Our kitchen is
             committed to providing our guests with the best Italian Cuisine.
-          </Description>
+          </DescriptionParagraph>
           <StyledButton background >Read more</StyledButton>
         </DescriptionBox>
       </WrapperInner>
