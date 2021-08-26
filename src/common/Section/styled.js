2b1cce57033b57f90f9f3b151b8d7da05518fdd8
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-08-26 13:43:06 +0200

    fromatt code and add styles

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,45 +1,57 @@
 import styled, { css } from "styled-components";
 
 export const Image = styled.img`
-max-width:400px;
+  max-width: 400px;
 
-${({aboutImage}) => aboutImage && css`
-max-width:700px;`}
+  ${({ aboutImage }) =>
+    aboutImage &&
+    css`
+      max-width: 700px;
+    `}
 `;
 
-export const LastImage= styled.img`
-width:355px;
+export const LastImage = styled.img`
+  width: 355px;
 `;
 export const ImagesBox = styled.div`
-display:flex;
-flex-direction:row;
-justify-content:space-between;
-margin-bottom:50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 50px;
 `;
 
 export const Wrapper = styled.section`
-margin:0 auto;
-max-width:1200px;
-text-align:center;
+  margin: 0 auto;
+  max-width: 1200px;
+  text-align: center;
 `;
 
 export const Description = styled.p`
   font-size: 18px;
   line-height: 1.5;
-  text-align:center;
-  color:#71726E;
-  margin:30px;
+  text-align: center;
+  color: #71726e;
+  margin: 30px;
 
-  ${({sentence}) => sentence && css`
-  font-size:25px;
-  margin:0px;
-  max-width:500px;
-  `}
-  `;
+  ${({ sentence }) =>
+    sentence &&
+    css`
+      font-size: 25px;
+      margin: 0px;
+      max-width: 500px;
+    `}
+
+  ${({ forAbout }) =>
+    forAbout &&
+    css`
+      text-align: left;
+      margin: 5px 0 20px 5px;
+      font-weight: 100;
+    `}
+`;
 export const NameImages = styled.p`
-color:#0914CE;
-font-size:22px;
+  color: #0914ce;
+  font-size: 22px;
 `;
 
-export const SingleImageBox = styled.div`
-`;
+export const SingleImageBox = styled.div``;
