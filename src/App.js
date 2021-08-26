4d46a8c13cbf3d9596b0964dd77bf8532ea11fa6
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-08-26 14:03:21 +0200

    create app.js add react router and import files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { Navigation } from "./common/Navigation/index";
+import { Home } from "./Home";
+import { AboutUs } from "./About";
+import { toHome, toAbout, toContact, toGallery, toMenu } from "./routes";
+
+export const App = () => (
+  <HashRouter>
+    <Navigation />
+    <Switch>
+      <Route path={toHome()}>
+        <Home />
+      </Route>
+      <Route path={toAbout()}>
+        <AboutUs />
+      </Route>
+      <Route path={toMenu()}></Route>
+      <Route path={toGallery()}></Route>
+      <Route path={toContact()}></Route>
+      <Route>
+        <Redirect to={toHome()} />
+      </Route>
+    </Switch>
+  </HashRouter>
+);
