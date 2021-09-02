b48f4043263ef8dfc941aff8b13825df633d1442
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-09-02 12:04:20 +0200

    add id and change on array with objects

---
+++ src/features/Menu/menuData.js
@@ -0,0 +1,104 @@
+export const menuData = [
+  {
+    name: "Mains",
+    id: "mains",
+    resources: [
+      {
+        name: "Osso Buco",
+        id:1,
+        price: 20.79,
+        description:
+          "Osso Buco is one of the Italian greats - slow cooked veal in a white wine tomato sauce. Meltingly tender, this is both hearty and luxurious.",
+      },
+      {
+        name: "Pappardelle Mimmo",
+        id:2,
+        price: 16.89,
+        description:
+          " This delicious dish tops long, wide pasta with scallops, lobster, asparagus, butter, sage and truffle oil to cater every palate.",
+      },
+      {
+        name: "Trippa Satriano",
+        id:3,
+        price: 18.69,
+        description:
+          "Thinly sliced herb encrusted ahi tuna topped with diced tomatoes, olives, capers, red onions and fennel. Perfect choice even for the first-time visitors!",
+      },
+      {
+        name: "Filetto Di Manzo",
+        id:4,
+        price: 19.5,
+        description:
+          "Wonderful combination of prime tenderloin, winter greens, Jerusalem artichoke puree, and oxtail reduction sauce.",
+      },
+    ],
+  },
+  {
+    name: "Desserts",
+    id: "desserts",
+    resources: [
+      {
+        name: "Tiramusu",
+        id:5,
+        price: 25,
+        description:
+          "A Pesto’s favorite - classic Italian dessert made with lady fingers, Mascarpone cheese & espresso. Perfect for both kids and adults.",
+      },
+      {
+        name: "Cannoli",
+        id:6,
+        price: 6.89,
+        description:
+          "Trio tower of cannoli filled with smooth ricotta, sugar & cinnamon, with chocolate & raspberry sauces. Single cannoli is also available.",
+      },
+      {
+        name: "Pistachio Passion",
+        id:7,
+        price: 5.69,
+        description:
+          "Layered pistachio cream, cream cheese custard & whipped cream atop a rich walnut crust.",
+      },
+      {
+        name: "Chocolate and Pistachio Biscotti",
+        id:8,
+        price: 4.8,
+        description:
+          "At Pesto, we vary these wonderful nutty biscotti, while also dipping them in melted dark chocolate for an extra layer of flavor.",
+      },
+    ],
+  },
+  {
+    name: "Drinks",
+    id: "drinks",
+    resources: [
+      {
+        name: "Aperol Spritz",
+        id:9,
+        price: 10.89,
+        description:
+          "The most popular drink in Venice: refreshing, easygoing &…happy! Perfect to be sipped as an “Aperitivo” just before dinner - delightful!",
+      },
+      {
+        name: "Negroni",
+        id:10,
+        price: 9.89,
+        description:
+          "Reward yourself with a moment of relaxation & pure pleasure while enjoying the full flavour & simplicity of a Negroni, an iconic Italian cocktail.",
+      },
+      {
+        name: "Negroni Sbagliato",
+        id:11,
+        price: 11.69,
+        description:
+          "A cocktail for those who prefer more delicate flavours but nonetheless want a drink full of taste & personality.",
+      },
+      {
+        name: "White Peach Bellini",
+        id:12,
+        price: 8.8,
+        description:
+          "White Peach Bellini is a classic drink from Venice Italy of white peach purée and Prosecco. It is one of our most popular drinks at Pesto.",
+      },
+    ],
+  },
+];
