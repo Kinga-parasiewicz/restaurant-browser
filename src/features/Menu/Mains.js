1e3245479fc19a64400f69d86da23600053aba57
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-09-02 15:42:25 +0200

    add styles to Mains.js

--- src/features/Menu/Mains.js
+++ src/features/Menu/Mains.js
@@ -1,22 +1,28 @@
 import { useParams } from "react-router-dom";
 import { menuData } from "./menuData";
+import {
+  ContainerDish,
+  BoxName,
+  Price,
+  FoodName,
+  FoodDescription,
+} from "./styled";
 
 export const Mains = () => {
   let { topicId } = useParams();
 
-  const typeMenu = menuData.find(({id}) => id === topicId)
+  const typeMenu = menuData.find(({ id }) => id === topicId);
   return (
-    <div>
-  
-       <div>
-        {typeMenu.resources.map(({price, description, name}) => (
-         <>
-          <h2>{name}</h2>
-          <h3>$ {price}</h3>
-          <p>{description}</p>
-         </>
-        ))}
-       </div>
-    </div>
+    <ContainerDish>
+      {typeMenu.resources.map(({ price, description, name }) => (
+        <>
+          <BoxName>
+            <FoodName>{name}</FoodName>
+            <Price>$ {price}</Price>
+          </BoxName>
+          <FoodDescription>{description}</FoodDescription>
+        </>
+      ))}
+    </ContainerDish>
   );
 };
