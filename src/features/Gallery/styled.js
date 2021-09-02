d689dff2fc60c5c6230f0ef2b1da3484c33c40ac
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-09-02 17:01:29 +0200

    add styles

---
+++ src/features/Gallery/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const BoxContent = styled.img`
+width: 500px;
+`;
+
+export const BoxImages =styled.div`
+text-align: center;
+max-width: 1400px;
+margin:120px auto;
+
+`;
