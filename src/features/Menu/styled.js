731b6bf326d641eb3b6cacdb75c3cf046e729069
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-09-02 12:21:16 +0200

    create styles to Menu component and Mains

---
+++ src/features/Menu/styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+export const WrapperMenu = styled.div`
+margin-top:160px;
+margin-bottom:120px;`;
+
+export const NavigationList = styled.ul`
+display:flex;
+flex-direction: row;
+justify-content:space-around;
+align-items:center;
+background-color: #323539;
+`;
+
+export const NavigationItem = styled.li`
+list-style-type: none;
+text-decoration: none;
+padding:20px
+`;
