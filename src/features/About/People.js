54b5aaec1c3b1ceb6800878b7b571b0214170e8c
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-09-06 17:17:27 +0200

    create array with content for Testimonials

---
+++ src/features/About/People.js
@@ -0,0 +1,35 @@
+import person1 from "./pobrane.jpeg";
+import person2 from "./person.jpeg";
+import person3 from "./person2.jpg";
+import person4 from "./person3.jpg";
+
+export const People = [
+  {
+    name: "Joanna Smith",
+    id: 0,
+    image: person1,
+    description:
+      "Morbi mollis risus ante, ac interdum elit mattis ut. Cras odio nunc, mattis ac tempor id, faucibus quis neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam ultrices faucibus libero, vel convallis sem dictum vel. Nulla ut ornare eros.",
+  },
+  {
+    name: "Kate McMillan",
+    id: 1,
+    image: person2,
+    description:
+      "Phasellus at auctor tortor, vitae lobortis nunc. Cras pulvinar nec lorem a vulputate. Sed tempus ultricies ipsum sed hendrerit. Fusce dignissim consectetur libero. Cras orci diam, blandit quis magna placerat, gravida iaculis mauris. Aenean eu erat eget mi commodo viverra vitae et turpis. Donec odio elit, posuere ut volutpat non, hendrerit ac felis.",
+  },
+  {
+    name: " John Williams",
+    id: 2,
+    image: person3,
+    description:
+      "Integer sapien eros, molestie non tortor sed, rutrum pulvinar odio. Morbi elementum id urna sit amet dapibus. Ut lobortis magna sed metus condimentum placerat. Aenean rhoncus lorem nec venenatis dapibus. Aliquam finibus, neque sit amet sodales sollicitudin, massa mauris tempus diam, eget volutpat arcu orci sed ex.  ",
+  },
+  {
+    name: "James Wilson",
+    id: 3,
+    image: person4,
+    description:
+      "Morbi massa sem, mattis quis augue et, gravida molestie felis. Pellentesque congue eu mauris nec dapibus. Nam vel erat eros. Maecenas ac erat egestas, accumsan purus eu, laoreet nulla. In turpis diam, tristique vitae nulla sed, vehicula accumsan magna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.  ",
+  },
+];
