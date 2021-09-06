bd5a15856d258cb31de3556b8d590cc047ea6914
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-09-06 17:17:36 +0200

    add styles

--- src/features/About/About-partOfPage/styled.js
+++ src/features/About/About-partOfPage/styled.js
@@ -1,26 +1,22 @@
 import styled, { css } from "styled-components";
+import pasta from "./../../../assets/background.jpg";
 
 export const Wrapper = styled.section`
   background: #efeeeb;
   margin-top: 20px;
 
-
   ${({ aboutWrapper }) =>
     aboutWrapper &&
     css`
       margin-top: 0px;
-      padding:30px;
+      padding: 30px;
       margin-bottom: 0px;
       background-color: white;
 
-      @media(max-width:768px){
-      width: 100%;
-    }
-
-
+      @media (max-width: 768px) {
+        width: 100%;
+      }
     `}
-
-   
 `;
 
 export const WrapperInner = styled.div`
@@ -34,24 +30,33 @@ export const WrapperInner = styled.div`
     paddingWrapper &&
     css`
       padding-top: 50px;
-     
     `}
 
-    @media(max-width:768px){
-     flex-direction: column;
-     
-    }
+  @media(max-width:768px) {
+    flex-direction: column;
+  }
 `;
-
 
 export const Name = styled.h2`
   font-size: 40px;
   text-align: center;
 
-  @media(max-width:768px){
+  @media (max-width: 768px) {
     text-align: center;
     font-size: 30px;
   }
+
+  ${({ colorName }) =>
+    colorName &&
+    css`
+      padding-top: 100px;
+      font-size: 48px;
+      color: white;
+
+      @media (max-width: 768px) {
+        padding-top: 60px;
+      }
+    `}
 `;
 
 export const DescriptionParagraph = styled.p`
@@ -65,18 +70,28 @@ export const DescriptionParagraph = styled.p`
       width: 300px;
     `}
 
-    @media(max-width:768px){
-      max-width: 300px;
-      margin: 10px auto;
-    }
+  ${({ colorDescription }) =>
+    colorDescription &&
+    css`
+      color: white;
+      max-width: 800px;
+      margin: 35px auto;
+      font-size: 24px;
+      text-align: center;
+    `}
+
+  @media(max-width:768px) {
+    max-width: 300px;
+    margin: 10px auto;
+  }
 `;
 export const DescriptionBox = styled.div`
   margin-left: 70px;
   text-align: center;
 
-  @media(max-width:768px){
+  @media (max-width: 768px) {
     margin-left: 0px;
-    margin:10px;
+    margin: 10px;
   }
 `;
 
@@ -103,3 +118,68 @@ export const Container = styled.div`
   margin: 30px auto;
 `;
 
+export const Testimonials = styled.section`
+  height: 80vh;
+  position: relative;
+  background-color: rgba(0, 0, 0, 0.7);
+  @media (max-width: 768px) {
+    height: 100vh;
+  }
+
+  &::before {
+    background-image: url(${pasta});
+    background-size: cover;
+    content: " ";
+    height: 80vh;
+    width: 100vw;
+    position: absolute;
+    z-index: -1;
+
+    @media (max-width: 768px) {
+      height: 100vh;
+      width: 100vw;
+    }
+  }
+`;
+export const TestimonialBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const TestimonialImage = styled.img`
+  height: 60px;
+  border-radius: 50%;
+  margin-top: 360px;
+  margin-right: 20px;
+  cursor: pointer;
+  position: relative;
+  transform-origin: bottom 100px;
+  transition: box-shadow 1s;
+
+  ${({ otherImage }) =>
+    otherImage &&
+    css`
+      height: 60px;
+      width: 57px;
+      border-radius: 50%;
+    `}
+
+  &:hover {
+    box-shadow: 2px 1px 2px 1px white;
+  }
+`;
+export const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 20px auto;
+  position: absolute;
+
+  &.active {
+    display: inline-block;
+  }
+  &.inactive {
+    display: none;
+  }
+`;
