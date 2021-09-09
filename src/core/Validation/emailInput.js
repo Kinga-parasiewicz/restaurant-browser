2c2cb69602362cf0dd2f1488e46595e6530fd125
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-09-09 16:48:13 +0200

    create emailInput.js

---
+++ src/core/Validation/emailInput.js
@@ -0,0 +1,33 @@
+import { Description } from "../Section/styled";
+import {
+  FormLabel,
+  FormInput,
+  FormWrapper,
+} from "./../../features/Form/styled";
+
+export const EmailInput = ({
+  label,
+  placeholder,
+  register,
+  validation,
+  value,
+  errors,
+  styledError,
+}) => {
+  return (
+    <FormWrapper>
+      <FormLabel>{label}:</FormLabel>
+      <FormInput
+        placeholder={placeholder}
+        type="email"
+        style={errors.email ? styledError.input : null}
+        {...register(value, { ...validation })}
+      ></FormInput>
+      {errors.email && (
+        <Description forForm style={styledError.message}>
+          {errors.email.message}
+        </Description>
+      )}
+    </FormWrapper>
+  );
+};
