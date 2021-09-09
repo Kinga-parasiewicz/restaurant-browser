7f84d67cbb6b4ab70c8c33bc58cfff44c203f8eb
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-09-09 11:08:01 +0200

    create styles

--- src/core/Navigation/styled.js
+++ src/core/Navigation/styled.js
@@ -6,16 +6,21 @@ export const StyledNavigation = styled.nav`
   top: 0;
   right: 0;
   left: 0;
-  background: rgba(255, 255, 255, 0.3);
+  background: rgba(255, 255, 255, 0.5);
   z-index: 1;
-
 `;
 
 export const NavigationInner = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
+
+  @media (max-width: 1024px) {
+    justify-content: space-between;
+    align-items: center;
+    margin: 0px auto;
+  }
 `;
 export const NameRestaurant = styled.h2`
   font-family: "Style Script", cursive;
@@ -47,10 +52,41 @@ export const ListItem = styled.li`
   text-transform: uppercase;
   font-size: 20px;
   margin-right: 50px;
+
+  @media (max-width: 1024px) {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
 `;
 
 export const NavigationList = styled.ul`
   display: flex;
+  align-items: center;
+
+  @media (max-width: 1024px) {
+    &.inactive {
+      position: fixed;
+      opacity: 0;
+      pointer-events: none;
+    }
+
+    &.active {
+      padding-top: 40px;
+      margin-top: 0px;
+      position: fixed;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      flex-direction: column;
+      align-items: center;
+      text-align: center;
+      background: white;
+      opacity: 1;
+      pointer-events: unset;
+      transform: none;
+    }
+  }
 `;
 
 export const Header = styled.header`
@@ -69,6 +105,11 @@ export const StyledLink = styled(Link)`
   &:hover {
     color: green;
     border-bottom: 2px solid green;
+
+    @media (max-width: 1024px) {
+      border-bottom: unset;
+      font-weight: 600;
+    }
   }
 
   ${({ secondary }) =>
@@ -84,5 +125,22 @@ export const StyledLink = styled(Link)`
       }
     `}
 `;
+
+export const HamburgerButton = styled.button`
+  display: none;
+
+  @media (max-width: 1024px) {
+    display: unset;
+    background: none;
+    border: none;
+    padding: 0;
+  }
+`;
+
 export const Hamburger = styled.img`
-display:none;`;
+  @media (max-width: 1024px) {
+    display: block;
+    margin: 10px;
+    width: 40px;
+  }
+`;
