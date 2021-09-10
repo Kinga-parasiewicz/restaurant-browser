e59476266b24441b86c9404cced07ddbf003a86c
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-09-10 12:23:25 +0200

    add useState and Information.js and create function

--- src/features/Form/index.js
+++ src/features/Form/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { StyledButton } from "./../../common/Button";
 import { StyledForm, FormLabel, FormInput, Select } from "./styled";
 import { useForm } from "react-hook-form";
@@ -8,57 +9,71 @@ import {
 } from "./../../core/Validation/validation";
 import { styledError } from "./../../core/Validation/styledError";
 import { NumberInput } from "../../core/Validation/numberInput";
+import { Information } from "./Information";
 
 export const Form = () => {
+  const [isActive, setActive] = useState("inactivefirst");
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const handleToggleClass = () => {
+    setActive(isActive === "inactivefirst" ? "active" : "inactivefirst");
+  };
+
+  const onSubmit = () => {
+    handleToggleClass();
+    reset();
+  };
 
   return (
     <>
-      <StyledForm onSubmit={handleSubmit(onSubmit)}>
-        <TextInput
-          label="Name*"
-          placeholder="your name"
-          register={register}
-          value="item"
-          validation={itemValidation}
-          errors={errors}
-          styledError={styledError}
-        />
-        <NumberInput
-          label="Phone*"
-          placeholder="your number"
-          value="number"
-          register={register}
-          validation={numberValidation}
-          errors={errors}
-          styledError={styledError}
-        />
-        <FormLabel>
-          Date*:
-          <FormInput type="date" />
-        </FormLabel>
-        <FormLabel>
-          No. Of People*:
-          <Select>
-            <option> </option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>6</option>
-            <option>7</option>
-            <option>8</option>
-          </Select>
-        </FormLabel>
-        <StyledButton background>check availability</StyledButton>
-      </StyledForm>
+      {isActive === "active" ? (
+        <Information />
+      ) : (
+        <StyledForm className={`${isActive}`} onSubmit={handleSubmit(onSubmit)}>
+          <TextInput
+            label="Name*"
+            placeholder="your name"
+            register={register}
+            value="item"
+            validation={itemValidation}
+            errors={errors}
+            styledError={styledError}
+          />
+          <NumberInput
+            label="Phone*"
+            placeholder="your number"
+            value="number"
+            register={register}
+            validation={numberValidation}
+            errors={errors}
+            styledError={styledError}
+          />
+          <FormLabel>
+            Date*:
+            <FormInput type="date" />
+          </FormLabel>
+          <FormLabel>
+            No. Of People*:
+            <Select>
+              <option> </option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+            </Select>
+          </FormLabel>
+          <StyledButton background>book now</StyledButton>
+        </StyledForm>
+      )}
     </>
   );
 };
