41ab1af2c981b6ebf08b288db3af1d78e07b9235
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-09-10 12:22:36 +0200

    create Information.js

---
+++ src/features/Form/Information.js
@@ -0,0 +1,12 @@
+import { Description, Wrapper } from "../../core/Section/styled";
+
+export const Information = () => {
+  return (
+    <Wrapper>
+      <Description sentence>
+        Thank you for your booking! We'll call you back and confirm your
+        reservation.
+      </Description>
+    </Wrapper>
+  );
+};
