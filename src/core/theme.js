1c4d9993bc9ba57237524e095a67b68dfedc90ed
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-09-16 15:40:20 +0200

    add colors name and breakpoints

--- src/core/theme.js
+++ src/core/theme.js
@@ -1,4 +1,18 @@
 export const theme = {
-  colors: {},
-  breakpoints: {},
+  colors: {
+    white: "#FFFFFF",
+    shark: "#323539",
+    mineShaft: "#3e3c3c",
+    green: "#008000",
+    cararra: "#efeeeb",
+    alto: "#D9D9D9",
+    crimson: "#DC143C",
+    stormDust: "#71726e",
+    black: "#000000",
+  },
+  breakpoints: {
+    mobile: "767",
+    tablet: "768",
+    desktop: "1024",
+  },
 };
